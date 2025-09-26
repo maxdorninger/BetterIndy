bffa8c378568b57421b870e92912901c8ef088b4
maxDorninger <97409287+maxDorninger@users.noreply.github.com>
2025-09-26 18:39:06 +0200

    add rooms to teacher dropdown when booking hours

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -2,15 +2,40 @@ console.log("BetterIndy is running!");
 let hourData;
 fetch("https://indy.sz-ybbs.ac.at:8443/hour/", {credentials: "include"}).then(response => {
     if (response.ok) {
-        hourData = response.json()
+        response.json().then(data => {
+            hourData = data;
+        })
     }
 });
-
 document.addEventListener("click", function (event) {
     setTimeout(() => {
         let teacherDropdown = document.querySelector("select#teacher");
-        for(let option of teacherDropdown.children) {
-            option.textContent = "Servas";
+        let [day, month, year] = document.querySelector("h2").innerHTML.split(" ")[2].split(".");
+        let date = new Date(`${year}-${month}-${day}`);
+        let dayOfWeek;
+        switch (date.getDay()) {
+            case 1:
+                dayOfWeek = "Mo";
+                break;
+            case 3:
+                dayOfWeek = "Mi";
+                break;
+            case 5:
+                dayOfWeek = "Fr";
+                break;
         }
-    },0)
+
+        console.log("BetterIndy: detected date is", date);
+
+        for(let option of teacherDropdown.children) {
+            let rooms = new Set;
+            for(let hour of hourData) {
+                if(option.value === hour.fullname && hour.day === dayOfWeek) {
+                    rooms.add(hour.room);
+                }
+            }
+            if(option.innerHTML !== "Lehrer auswählen" )
+                option.innerHTML = `${option.value} //  ${rooms.values().toArray().join(", ")}`;
+        }
+    },100)
 });
